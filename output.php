c4f6700f97c14cf929198c0e48c7c17a3e06fc5a
Gagas Putra Persada <42725322+gagasputra@users.noreply.github.com>
2018-10-01 10:49:39 +0700

    Add files via upload

---
+++ output.php
@@ -0,0 +1,16 @@
+<table>
+    <th> NIM </th>
+    <th> Nama </th>
+    <th> Email </th>
+    <tr>
+        <td>
+            <?php echo $_POST['nim']; ?>
+        </td>
+        <td>
+            <?php echo $_POST['nama']; ?>
+        </td>
+        <td>
+            <?php echo $_POST['email']; ?>
+        </td>
+    </tr>
+</table>
