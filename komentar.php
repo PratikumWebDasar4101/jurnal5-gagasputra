99101f744a588847e8627139c54d4b0f2bdc9544
Gagas Putra Persada <42725322+gagasputra@users.noreply.github.com>
2018-10-01 09:52:38 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> KOMENTAR </title>
+</head>
+<body>
+    <pre>
+        <form method="POST">
+            Nama        : <input type="text" name="nama" ><br><br>
+            Komentar    : <br>
+            <textarea name="komentar" cols="30" rows="10" placeholder="Komentar Minimal 5 Karakter"></textarea><br>
+            <input type="submit" value="KIRIM">
+        </form>
+    </pre>
+</body>
+</html>
+
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+    if (isset($_POST['nama'])) {
+        $nama = $_POST['nama'];
+        $komentar = $_POST['komentar'];
 
-
-
-
+        $jumlah = strlen($komentar);
+        if ($jumlah < 5) {
+            ?>
+            <script>
+                alert("ERROR!!! (KOMENTAR KURANG DARI 5 HURUF)")
+            </script>
+            <?php
+        } else {
+            ?> 
+            <pre>
+                <?php
+                    echo "Jumlah Huruf Pada Komentar : ".$jumlah." Huruf";
+                ?>
+            </pre>
+            <?php
+        }
+    }
 ?>
