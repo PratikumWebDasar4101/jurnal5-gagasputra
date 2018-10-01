eababe83e8f4e6ff98db07fb8e6a3fef6a5f93f9
Gagas Putra Persada <42725322+gagasputra@users.noreply.github.com>
2018-10-01 09:53:11 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Form Registrasi</title>
+    <title> Registrasi </title>
 </head>
 <body>
-
-
-
+    <pre>
+        <h2> REGISTRASI </h2>
+        <form method="POST">
+            NIM     : <input type="text" name="nim" pattern="\d*" maxlength="10"> <br><br>
+            Nama    : <input type="text" name="nama" maxlength="25"> <br><br>
+            Email   : <input type="email" name="email"> <br><br>    
+            <input type="submit" value="KIRIM">
+        </form>
+    </pre>
 </body>
 </html>
+
+<?php
+    if(isset($_POST['nim'])) {
+        $nama = $_POST['nama'];
+        $nim = $_POST['nim'];
+        $email = $_POST['email'];
+    }
+?> 
+    <script>
+        alert("Registrasi Berhasil")
+    </script>
